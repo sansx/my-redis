2ff10dcbbe402ea64a8fb7238b3b359a4bd3723d
sansx <xiathxth@gmail.com>
2020-12-12 23:20:58 +0800

    mini-tokio

---
+++ examples/io/echo-server.rs
@@ -0,0 +1,68 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut listener = TcpListener::bind("127.0.0.1:6142").await.unwrap();
+
+    loop {
+        tokio::spawn(async move {
+            process().await.unwrap();
+        });
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+            // process().await.unwrap();
+            loop {
+                match socket.read(&mut buf).await {
+                    // Return value of `Ok(0)` signifies that the remote has
+                    // closed
+                    Ok(0) => return,
+                    Ok(n) => {
+                        // Copy the data back to socket
+                        if socket.write_all(&buf[..n]).await.is_err() {
+                            // Unexpected socket error. There isn't much we can
+                            // do here so just stop processing.
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        // Unexpected socket error. There isn't much we can do
+                        // here so just stop processing.
+                        return;
+                    }
+                }
+            }
+        });
+    }
+}
+
+async fn process() -> io::Result<()> {
+    let socket = TcpStream::connect("127.0.0.1:6142").await?;
+    let (mut rd, mut wr) = io::split(socket);
+
+    // Write data in the background
+    let write_task = tokio::spawn(async move {
+        wr.write_all(b"hello\r\n").await?;
+        wr.write_all(b"world\r\n").await?;
+
+        // Sometimes, the rust type inferencer needs
+        // a little help
+        Ok::<_, io::Error>(())
+    });
+
+    let mut buf = vec![0; 128];
+
+    loop {
+        let n = rd.read(&mut buf).await?;
+
+        if n == 0 {
+            break;
+        }
+
+        println!("GOT {:?}", &buf[..n]);
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,108 @@
-use tokio::io::{self, AsyncWriteExt};
-use tokio::fs::File;
+use std::collections::VecDeque;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+use futures::task;
+use std::thread;
+use crossbeam::channel;
+use std::sync::{Arc, Mutex};
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let mut buffer = File::create("foo1.txt").await?;
+struct Delay {
+  when: Instant,
+}
 
-    buffer.write_all(b"some bytes").await?;
-    Ok(())
+impl Future for Delay {
+  type Output = &'static str;
+
+  fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
+      -> Poll<&'static str>
+  {
+      if Instant::now() >= self.when {
+          println!("Hello world");
+          Poll::Ready("done")
+      } else {
+          // Get a handle to the waker for the current task
+          let waker = cx.waker().clone();
+          let when = self.when;
+
+          // Spawn a timer thread.
+          thread::spawn(move || {
+              let now = Instant::now();
+
+              if now < when {
+                  thread::sleep(when - now);
+              }
+
+              waker.wake();
+          });
+
+          Poll::Pending
+      }
+  }
+}
+
+fn main() {
+    let mut mini_tokio = MiniTokio::new();
+
+    mini_tokio.spawn(async {
+        let when = Instant::now() + Duration::from_millis(10);
+        let future = Delay { when };
+
+        let out = future.await;
+        assert_eq!(out, "done");
+    });
+
+    mini_tokio.run();
+}
+
+struct MiniTokio {
+  scheduled: channel::Receiver<Arc<Task>>,
+  sender: channel::Sender<Arc<Task>>,
+}
+
+
+
+struct Task {
+    // The `Mutex` is to make `Task` implement `Sync`. Only
+    // one thread accesses `future` at any given time. The
+    // `Mutex` is not required for correctness. Real Tokio
+    // does not use a mutex here, but real Tokio has
+    // more lines of code than can fit in a single tutorial
+    // page.
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    executor: channel::Sender<Arc<Task>>,
+}
+
+impl Task {
+    fn schedule(self: &Arc<Self>) {
+        self.executor.send(self.clone());
+    }
+}
+
+impl MiniTokio {
+    fn new() -> MiniTokio {
+        MiniTokio {
+            tasks: VecDeque::new(),
+        }
+    }
+    
+    /// Spawn a future onto the mini-tokio instance.
+    fn spawn<F>(&mut self, future: F)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        self.tasks.push_back(Box::pin(future));
+    }
+    
+    fn run(&mut self) {
+        let waker = task::noop_waker();
+        let mut cx = Context::from_waker(&waker);
+        
+        while let Some(mut task) = self.tasks.pop_front() {
+            if task.as_mut().poll(&mut cx).is_pending() {
+                self.tasks.push_back(task);
+            }
+        }
+    }
 }
